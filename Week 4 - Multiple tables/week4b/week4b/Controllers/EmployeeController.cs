decb3465e77e7c6942c390cee954ead00961be9b
LeeS <leejsitu@gmail.com>
2017-02-02 11:53:36 -0500

    Added week4b

---
+++ Week 4 - Multiple tables/week4b/week4b/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using week4b.Models;
+
+namespace week4b.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        CompanyContext companyContext = new CompanyContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            //List of all employees in db
+            List<Employee> employees = companyContext.Employees.ToList();
+            return View(employees);
+        }
+
+        //GET: one Employee
+        public ActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                Employee employee = companyContext.Employees.Single(emp => emp.Id == id);
+                return View(employee);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Week 4 - Multiple tables/week4b/week4b/Models/CompanyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace week4b.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
